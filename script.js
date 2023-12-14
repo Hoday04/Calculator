2ba45c2946585d02aea6905fa8e26ef9844eb10d
Hoday04 <103636616+Hoday04@users.noreply.github.com>
2023-12-14 14:35:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function deleteLastDigit() {
+    let result = document.getElementById("result").value;
+    document.getElementById("result").value = result.slice(0, -1);
+  }
+  
+  function appendSymbol(symbol) {
+    document.getElementById("result").value += symbol;
+  }
+  
+  function calculate() {
+    let result = document.getElementById("result").value;
+    try {
+      let answer = eval(result);
+      document.getElementById("result").value = answer;
+    } catch (error) {
+      document.getElementById("result").value = "Error";
+    }
+  }
